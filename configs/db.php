59d78d7d983d672a5a02a01ccccfa6338914b83f
loan018 <loanptph52441@gmail.com>
2025-07-10 17:21:31 +0700

    sửa lại lab 2

--- configs/db.php
+++ configs/db.php
@@ -1,10 +1,10 @@
 <?php
-require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/env.php';
 
 try {
     $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT;
     $pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD, DB_OPTIONS);
-    echo "Kết nối thành công";
+    //echo "Kết nối thành công";
 } catch (PDOException $e) {
     die("Kết nối CSDL thất bại: " . $e->getMessage());
 }
